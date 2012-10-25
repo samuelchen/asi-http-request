071c55a1201cff425664ab0f314df29f9ee527bf
Samuel Chen <samuel.net@gmail.com>
2012-10-25 17:49:52 +0800

    Add a project to build framework target

---
+++ Mac Library/Mac Library/Mac_Library.h
@@ -0,0 +1,13 @@
+//
+//  Mac_Library.h
+//  Mac Library
+//
+//  Created by Samuel on 12-10-25.
+//  Copyright (c) 2012年 Sohu Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mac_Library : NSObject
+
+@end
---
+++ Mac Library/Mac Library/Mac_Library.m
@@ -0,0 +1,13 @@
+//
+//  Mac_Library.m
+//  Mac Library
+//
+//  Created by Samuel on 12-10-25.
+//  Copyright (c) 2012年 Sohu Inc. All rights reserved.
+//
+
+#import "Mac_Library.h"
+
+@implementation Mac_Library
+
+@end
